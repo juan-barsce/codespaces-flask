faf5f7a792d00db709d97dd5d6c7b98982aa6c2c
juan-barsce <122497863+juan-barsce@users.noreply.github.com>
2023-03-16 02:43:19 +0000

    Commited latest changes in demo

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+app.secret_key = 'supersecret key'
+
+
+def predict(x):
+    return 'it\'s a dog'
+
+
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return render_template("index.html", title="Hello fellows")
+    if request.method == 'GET':
+        return render_template("index.html", title="Hello fellows")
+    if request.method == 'POST':
+        value_to_predict = request.form['value_to_predict']
+        predicted_class = predict(value_to_predict)
+
+        return render_template("index.html", title="Hello fellows", predicted=predicted_class)
